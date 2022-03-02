beed689541fa3c96e90eb38c230fcb28520c974c
Louise <louise.delrieu25@gmail.com>
2022-03-02 15:12:35 +0100

    [hangman] hangman correct

--- src/hangman/hangman.cpp
+++ src/hangman/hangman.cpp
@@ -2,6 +2,7 @@
 #include <array>
 #include <iostream>
 #include <string>
+#include <vector>
 #include "../commons/get_input_from_user.hpp"
 #include "../commons/rand.hpp"
 
@@ -9,24 +10,54 @@ const std::string pick_a_random_word()
 {
     const std::array<std::string, 5> words        = {"bonjour", "courgette", "biscuit", "plante", "cactus"};
     int                              random_index = rand<int>(0, words.size() - 1);
-    std::cout << words[random_index] << std::endl;
     return words[random_index];
+}
+
+bool is_user_input_is_in_word(char user_input, std::string word, std::string& display_word, unsigned int& letters_found)
+{
+    bool is_in_word;
+    for (unsigned int i = 0; i < word.size(); i++) {
+        if (word[i] == user_input) {
+            display_word[i] = user_input;
+            letters_found++;
+            is_in_word = true;
+        }
+    }
+    if (is_in_word) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void display_end_game(int& player_life)
+{
+    if (player_life == 0) {
+        std::cout << "Too bad your lost :(" << std::endl;
+    }
+    else {
+        std::cout << "Congrats, you won ! " << std::endl;
+    }
 }
 
 void play_hangman()
 {
     int               player_life = 8;
-    std::string       user_input;
-    bool              is_winning  = false;
-    const std::string random_word = pick_a_random_word();
-    bool              is_in_word  = false;
-    while (player_life > 0 && !is_winning) {
+    char              user_input;
+    unsigned int      letters_found = 0;
+    const std::string random_word   = pick_a_random_word();
+    std::string       display_word(random_word.size(), '_');
+    while (letters_found < random_word.size() && player_life > 0) {
         std::cout << "You have " << player_life << " lives." << std::endl;
-        for (auto it = random_word.begin(); it != random_word.end(); ++it) {
-            std::cout << "_";
-        }
+        std::cout << display_word;
         std::cout << std::endl;
-        user_input = get_input_from_user<std::string>();
-        player_life--;
+        user_input = get_input_from_user<char>();
+        if (!is_user_input_is_in_word(user_input, random_word, display_word, letters_found)) {
+            player_life--;
+        }
     }
+    std::cout << "The word is : " << random_word << std::endl;
+
+    display_end_game(player_life);
 }
--- src/hangman/hangman.hpp
+++ src/hangman/hangman.hpp
@@ -2,4 +2,6 @@
 #include <string>
 
 const std::string pick_a_random_word();
+bool              is_user_input_is_in_word(char user_input, std::string word, std::string& display_word, unsigned int& letters_found);
+void              display_end_game(int& player_life);
 void              play_hangman();

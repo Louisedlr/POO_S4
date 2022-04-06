25f2922119236f436467abd5c56654e47d5fec5d
Louise <louise.delrieu25@gmail.com>
2022-04-06 15:16:07 +0200

    [noughts and crosses] draw cross and draw nought

--- src/noughts_and_crosses/noughts_and_crosses.cpp
+++ src/noughts_and_crosses/noughts_and_crosses.cpp
@@ -10,10 +10,10 @@ float cell_radius(int board_size)
 
 void draw_cell(CellIndex index, int board_size, p6::Context& ctx)
 {
-    ctx.square(p6::BottomLeftCorner{p6::map(glm::vec2{static_cast<float>(index.x), static_cast<float>(index.y)},
-                                            glm::vec2{0.f}, glm::vec2{static_cast<float>(board_size)},
+    ctx.square(p6::BottomLeftCorner{p6::map(glm::vec2{float(index.x), float(index.y)},
+                                            glm::vec2{0.f}, glm::vec2{float(board_size)},
                                             glm::vec2{-1.f}, glm::vec2{1.f})},
-               p6::Radius{1.f / static_cast<float>(board_size)});
+               p6::Radius{1.f / float(board_size)});
 }
 
 void draw_board(int size, p6::Context& ctx)
@@ -29,10 +29,10 @@ std::optional<CellIndex> cell_hovered_by(glm::vec2 position, int board_size)
 {
     const auto pos   = p6::map(position,
                              glm::vec2{-1.f}, glm::vec2{1.f},
-                             glm::vec2{0.f}, glm::vec2{static_cast<float>(board_size)});
+                             glm::vec2{0.f}, glm::vec2{float(board_size)});
     const auto index = CellIndex{
-        static_cast<int>(std::floor(pos.x)),
-        static_cast<int>(std::floor(pos.y))};
+        int(std::floor(pos.x)),
+        int(std::floor(pos.y))};
     if (index.x >= 0 && index.x < board_size &&
         index.y >= 0 && index.y < board_size) {
         return std::make_optional(index);
@@ -40,6 +40,40 @@ std::optional<CellIndex> cell_hovered_by(glm::vec2 position, int board_size)
     else {
         return std::nullopt;
     }
+}
+glm::vec2 cell_bottom_left_corner(CellIndex index, int board_size)
+{
+    const auto idx = glm::vec2{static_cast<float>(index.x),
+                               static_cast<float>(index.y)};
+    return p6::map(idx,
+                   glm::vec2{0.f}, glm::vec2{static_cast<float>(board_size)},
+                   glm::vec2{-1.f}, glm::vec2{1.f});
+}
+
+glm::vec2 cell_center(CellIndex index, int board_size)
+{
+    return cell_bottom_left_corner(index, board_size) + cell_radius(board_size);
+}
+
+void draw_nought(CellIndex index, int board_size, p6::Context& ctx)
+{
+    ctx.stroke        = {74 / 255.f, 137 / 255.f, 255 / 255.f, 1.f};
+    ctx.fill          = {74 / 255.f, 137 / 255.f, 255 / 255.f, 1.f};
+    ctx.stroke_weight = 0.4f * cell_radius(board_size);
+    ctx.circle(p6::Center{cell_center(index, board_size)},
+               p6::Radius{0.9f * cell_radius(board_size)});
+}
+
+void draw_cross(CellIndex index, int board_size, p6::Context& ctx)
+{
+    ctx.stroke          = {74 / 255.f, 137 / 255.f, 255 / 255.f, 1.f};
+    ctx.fill            = {74 / 255.f, 137 / 255.f, 255 / 255.f, 1.f};
+    ctx.stroke_weight   = 0.4f * cell_radius(board_size);
+    const auto center   = p6::Center{cell_center(index, board_size)};
+    const auto radii    = p6::Radii{glm::vec2{1.f, 0.2f} * cell_radius(board_size)};
+    const auto rotation = p6::Rotation{0.125_turn};
+    ctx.rectangle(center, radii, rotation);
+    ctx.rectangle(center, radii, -rotation);
 }
 
 void play_noughts_and_crosses()
@@ -55,7 +89,8 @@ void play_noughts_and_crosses()
         const auto hovered_cell = cell_hovered_by(ctx.mouse(), board_size);
         if (hovered_cell.has_value()) {
             ctx.fill = {74 / 255.f, 137 / 255.f, 255 / 255.f, 1.f};
-            draw_cell(*hovered_cell, board_size, ctx);
+            // draw_cell(*hovered_cell, board_size, ctx);
+            draw_cross(*hovered_cell, board_size, ctx);
         }
     };
     ctx.start();
--- src/noughts_and_crosses/noughts_and_crosses.hpp
+++ src/noughts_and_crosses/noughts_and_crosses.hpp
@@ -11,5 +11,10 @@ float                    cell_radius(int board_size);
 void                     draw_board(int size, p6::Context& ctx);
 void                     draw_cell(CellIndex index, int board_size, p6::Context& ctx);
 std::optional<CellIndex> cell_hovered_by(glm::vec2 position, int board_size);
+glm::vec2                cell_bottom_left_corner(CellIndex index, int board_size);
+glm::vec2                cell_center(CellIndex index, int board_size);
+
+void draw_nought(CellIndex index, int board_size, p6::Context& ctx);
+void draw_cross(CellIndex index, int board_size, p6::Context& ctx);
 
 void play_noughts_and_crosses();
